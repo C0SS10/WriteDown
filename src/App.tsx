521e895d545ca7748422c1b1ad1ac0ddf18aac9f
Esteban <estebancg12@hotmail.com>
2025-01-03 17:45:08 -0500

    Refactor App component to use React Router and add Home page 🧰

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-export default function App() {
-  const users = ["Gonzalez", "Carlos", "Luis", "Miguel", "Esteban", "Cossio"];
-  const usersSorted = users.sort();
-  const userByLetter = Object.groupBy(usersSorted, (user) => user[0]);
-  console.log(userByLetter);
+import { Route, BrowserRouter as Router, Routes } from "react-router";
+import Home from "./pages/Home";
 
+export default function App() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center bg-slate-700">
-      <h1 className="text-gray-400">Inicial</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
