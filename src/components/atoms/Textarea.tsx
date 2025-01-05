ef510502f9b77cf2c41c3f4d5892c466ff1c6eb9
Esteban <estebancg12@hotmail.com>
2025-01-05 11:11:54 -0500

    Add Textarea component for user input in the editor ➕

---
+++ src/components/atoms/Textarea.tsx
@@ -0,0 +1,9 @@
+export const Textarea = () => {
+  return (
+    <textarea
+      name="text"
+      placeholder="# Esto es un título"
+      className="bg-slate-400/10 border border-slate-200/20 shadow-md resize-none rounded-md p-4 w-[45.8rem] h-[36rem] text-2xl text-slate-200 outline-none mx-4"
+    ></textarea>
+  );
+};
