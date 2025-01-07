28762c6cc6638528de31c151fb64587b4f5c75fd
Esteban <estebancg12@hotmail.com>
2025-01-07 10:22:14 -0500

    Update Textarea component styles 🎨

--- src/components/atoms/Textarea.tsx
+++ src/components/atoms/Textarea.tsx
@@ -3,7 +3,7 @@ export const Textarea = () => {
     <textarea
       name="text"
       placeholder="# Esto es un título"
-      className="bg-slate-400/10 border border-slate-200/20 shadow-md resize-none rounded-md p-4 w-[45.8rem] h-[36rem] text-2xl text-slate-200 outline-none mx-4"
+      className="bg-slate-400/10 border border-slate-200/20 shadow-md resize-none rounded-md p-4 w-[46rem] h-[36rem] text-xl text-slate-200 outline-none mx-4"
     ></textarea>
   );
 };
