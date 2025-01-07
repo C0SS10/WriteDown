667d6b4c28c28a7f932383d72733421f2bd80cb1
Esteban <estebancg12@hotmail.com>
2025-01-07 10:22:05 -0500

    Refactor component to accept dynamic data and orientation 🧰

--- src/components/molecules/ToolBar.tsx
+++ src/components/molecules/ToolBar.tsx
@@ -1,89 +1,25 @@
-import {
-  ALargeSmall,
-  Bold,
-  Code,
-  Heading1,
-  Heading2,
-  Heading3,
-  Image,
-  Italic,
-  List,
-  ListOrdered,
-  Strikethrough,
-} from "lucide-react";
-import { ToolButton } from "../atoms/ToolButton";
 import { ToolButtonProps } from "../../types/ToolButton";
+import { ToolButton } from "../atoms/ToolButton";
 
-export const ToolBar = () => {
-  const toolStyles = "h-10 w-10 text-gray-200";
+interface ToolBarProps {
+  tools: ToolButtonProps[];
+  isVertical?: boolean;
+}
 
-  const tools: ToolButtonProps[] = [
-    {
-      title: "Mayúscula",
-      icon: <ALargeSmall className={toolStyles} />,
-      isActive: false,
-    },
-    {
-      title: "Negrita",
-      icon: <Bold className={toolStyles} />,
-      isActive: false,
-    },
-    {
-      title: "Itálica",
-      icon: <Italic className={toolStyles} />,
-      isActive: false,
-    },
-    {
-      title: "Subrayado",
-      icon: <Strikethrough className={toolStyles} />,
-      isActive: false,
-    },
-    {
-      title: "Título",
-      icon: <Heading1 className={toolStyles} />,
-      isActive: false,
-    },
-    {
-      title: "Subtítulo",
-      icon: <Heading2 className={toolStyles} />,
-      isActive: false,
-    },
-    {
-      title: "Subitem",
-      icon: <Heading3 className={toolStyles} />,
-      isActive: false,
-    },
-    {
-      title: "Lista ordenada",
-      icon: <ListOrdered className={toolStyles} />,
-      isActive: false,
-    },
-    {
-      title: "Lista desordenada",
-      icon: <List className={toolStyles} />,
-      isActive: false,
-    },
-    {
-      title: "Código",
-      icon: <Code className={toolStyles} />,
-      isActive: false,
-    },
-    {
-      title: "Imagen",
-      icon: <Image className={toolStyles} />,
-      isActive: false,
-    },
-  ];
-
+export const ToolBar = ({ tools, isVertical }: ToolBarProps) => {
   return (
-    <section className="flex justify-center items-center gap-2 p-4 mx-4 bg-slate-400/10 backdrop-blur-md rounded-lg border border-slate-200/20 shadow-lg">
+    <section
+      className={`flex ${
+        isVertical ? "flex-col" : "flex-row p-4 mx-4"
+      } justify-center items-center gap-2 bg-slate-400/10 backdrop-blur-md rounded-lg border border-slate-200/20 shadow-lg`}
+    >
       {tools.map((tool, index) => (
         <ToolButton
           key={index}
           title={tool.title}
           icon={tool.icon}
           isActive={tool.isActive}
-          onClick={() => console.log(tool.title)}
+          onClick={tool.onClick}
         />
       ))}
     </section>
