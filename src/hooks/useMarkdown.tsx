9f9c9f22cd4ec29031c8d972346174f3b6d9e897
Esteban <estebancg12@hotmail.com>
2025-02-12 13:03:33 -0500

    Add custom hook useMarkdown for formatting text 📝

---
+++ src/hooks/useMarkdown.tsx
@@ -0,0 +1,44 @@
+import { useCallback } from "react";
+
+export function useMarkdown(setContent: (value: string) => void) {
+  const formatText = useCallback(
+    (symbol: string, wrapper?: boolean) => {
+      const markdownTextArea = document.getElementById(
+        "markdown-textarea"
+      ) as HTMLTextAreaElement;
+
+      if (!markdownTextArea) return;
+
+      const { selectionStart, selectionEnd, value } = markdownTextArea;
+      const selectedText = value.substring(selectionStart, selectionEnd);
+
+      let newText = "";
+
+      if (selectedText) {
+        newText = wrapper
+          ? `${symbol}${selectedText}${symbol}`
+          : `${symbol} ${selectedText}`;
+      } else {
+        newText = wrapper ? `${symbol} ${symbol}` : `${symbol}`;
+      }
+
+      const updateContent =
+        value.substring(0, selectionStart) +
+        newText +
+        value.substring(selectionEnd);
+
+      setContent(updateContent);
+
+      // Mover el cursor al final del texto
+      const cursorPosition = selectionStart + newText.length;
+      setTimeout(
+        () =>
+          markdownTextArea.setSelectionRange(cursorPosition, cursorPosition),
+        0
+      );
+    },
+    [setContent]
+  );
+
+  return { formatText };
+}
